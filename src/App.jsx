85b28812f388ddf1f954c9ea583c3933b83233c0
Arun <74652697+ArunS-tack@users.noreply.github.com>
2023-07-02 02:18:49 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -42,45 +42,43 @@ function TransactionDetails() {
     getTx();
   };
 
-
   return (
     <div className="App">
-      <Card className="text-center">
-        <Card.Header><h2>Transaction Visualizer</h2></Card.Header>
-        <Card.Body>
-          <Form onSubmit={handleSubmit}>
-            <Form.Group controlId="formBasicEmail" className="mb-3">
-              <Form.Label><h3>Transaction ID</h3></Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Enter transaction ID...  (Devnet)"
-                onChange={handleTxIdChange}
-              />
-            </Form.Group>
-            <Button variant="primary" type="submit">
-              Get Details 
-            </Button>
-          </Form>
-          <br />
-          {error && <Alert variant="danger">{error}</Alert>}
-          {TxData && (
-            <Card>
-              <Card.Body>
-                <Card.Title>Transaction Details</Card.Title>
-                <Card.Text>
-                  <p>Signature: {signature}</p>
-                  <p>Result: {TxData['meta']['err'] ? 'Failed' : 'Success'}</p>
-                  <p>Fee: {TxData['meta']['fee']/LAMPORTS_PER_SOL} SOL</p>
-                  <p>Slot: {Intl.NumberFormat().format(TxData['slot'])}</p>
-                  <p>Block Time: {new Date(TxData['blockTime']*1000).toLocaleString()}</p>
-                  </Card.Text>
-              </Card.Body>
-            </Card>
-          )}
-        </Card.Body>
-      </Card>
+      <div className="text-center">
+        <h2>Transaction Visualizer</h2>
+      </div>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-3">
+            <label><h3>Transaction ID</h3></label>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Enter transaction ID... (Devnet)"
+              onChange={handleTxIdChange}
+            />
+          </div>
+          <button className="btn btn-primary" type="submit">
+            Get Details
+          </button>
+        </form>
+        <br />
+        {error && <div className="alert alert-danger">{error}</div>}
+        {TxData && (
+          <div className='details'>
+            <h4>Transaction Details</h4>
+            <p>Signature: {signature}</p>
+            <p>Result: {TxData['meta']['err'] ? 'Failed' : 'Success'}</p>
+            <p>Fee: {TxData['meta']['fee'] / LAMPORTS_PER_SOL} SOL</p>
+            <p>Slot: {Intl.NumberFormat().format(TxData['slot'])}</p>
+            <p>Block Time: {new Date(TxData['blockTime'] * 1000).toLocaleString()}</p>
+          </div>
+        )}
+      </div>
     </div>
+
   );
+  
 }
 
 function App() {
